03f7477070ea2cf6cbc1e0e0c99c43a7fc80690f
marcel naujoks <jb3380@yahoo.de>
2012-06-21 18:39:13 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialhousing::Application.config.secret_token = '89217e23b364df71bf6b8973cab768f0156d6eff414112500ca6ba9939926e87e69f98e3ddf7427e141417b964542f7345b03d46bb76f10cb5e5182f0c22a773'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Socialhousing::Application.config.session_store :cookie_store, key: '_socialhousing_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Socialhousing::Application.config.session_store :active_record_store
