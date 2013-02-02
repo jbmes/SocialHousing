4bfef7d9500809b06e27b0528659052cc65c52e8
kostja <konstantin@silin.info>
2013-02-02 23:23:02 +0100

    adding and removing links works in UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,31 +14,32 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(document).ready(function(){
-	$('.btn_addLink').click(function () {
-	$("#pop").children("li").length
-	$('#pop').append("<div >link: <label> <input type = 'text' ><a href='JavaScript:void(0);' class='btn_removeLink'>Remove</a></label></div >");
-	});
-	
-	$('.btn_removeLink').live("click", function () {
-	$(this).parent().parent().remove()
-	});
+$(document).ready(function () {
+    $('.btn_addLink').click(function () {
+        $("#pop").children("li").length
+        $('#pop').append("<div >link: <label> <input type = 'text' ><a href='JavaScript:void(0);' class='btn_removeLink'>Remove</a></label></div >");
+    });
+
+    $('.btn_removeLink').live('click', function () {
+        $(this).parent().parent().remove();
+
+    });
 });
 
 
-$(function() {
-  if ($("#comments").length > 0) {
+$(function () {
+    if ($("#comments").length > 0) {
+        setTimeout(updateComments, 10000);
+    }
+});
+
+function updateComments() {
+    var article_id = $("#article").attr("data-id");
+    if ($(".comment").length > 0) {
+        var after = $(".comment:last-child").attr("data-time");
+    } else {
+        var after = "0";
+    }
+    $.getScript("/comments.js?article_id=" + article_id + "&after=" + after)
     setTimeout(updateComments, 10000);
-  }
-});
-
-function updateComments () {
-  var article_id = $("#article").attr("data-id");
-  if ($(".comment").length > 0) {
-    var after = $(".comment:last-child").attr("data-time");
-  } else {
-    var after = "0";
-  }
-  $.getScript("/comments.js?article_id=" + article_id + "&after=" + after)
-  setTimeout(updateComments, 10000);
 }
